ea2d7fe1acc3ec953521ee75353fad77b7160083
PedeLa <lange@dbis.rwth-aachen.de>
2015-07-21 18:50:57 +0200

    corrected unix script and changed variable name in start scripts

--- backend/bin/start_network.bat
+++ backend/bin/start_network.bat
@@ -7,5 +7,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.$Lower_Microservice_Name$.$Microservice_Name$') startWebConnector interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.$Lower_Resource_Name$.$Resource_Name$') startWebConnector interactive
 pause
