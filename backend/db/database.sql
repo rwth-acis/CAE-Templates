4617b722977e3d85d032ae65a22ded147165ce55
PedeLa <lange@dbis.rwth-aachen.de>
2015-08-21 10:39:06 +0200

    added database template

---
+++ backend/db/database.sql
@@ -0,0 +1,9 @@
+--
+-- Database:  $Database_Schema$ 
+-- Automatically generated sql script for the service $Service_Name$, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table $Database_Table_Name$.
+--
+$Database_Table$
