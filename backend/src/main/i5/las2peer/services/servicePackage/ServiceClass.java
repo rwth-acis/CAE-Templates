7bb195a77bd3f3ec6c84fa04a52f53db5eaec40d
Jasper Nalbach <nalbach@dbis.rwth-aachen.de>
2016-10-16 15:34:55 +0200

    fix imports

--- backend/src/main/i5/las2peer/services/servicePackage/ServiceClass.java
+++ backend/src/main/i5/las2peer/services/servicePackage/ServiceClass.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
