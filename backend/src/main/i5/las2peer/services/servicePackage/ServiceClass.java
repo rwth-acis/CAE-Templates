d88f7904f2e502cfa67f3c9ef74bd958e80fa62f
Philipp Hossner <philipp.hossner@posteo.de>
2019-07-15 18:11:47 +0200

    Fix missing imports

--- backend/src/main/i5/las2peer/services/servicePackage/ServiceClass.java
+++ backend/src/main/i5/las2peer/services/servicePackage/ServiceClass.java
@@ -1,7 +1,23 @@
 package i5.las2peer.services.$Lower_Resource_Name$;
 
+$Additional_Import$
+import java.net.HttpURLConnection;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
 import i5.las2peer.api.Context;
 import i5.las2peer.api.ManualDeployment;
+import i5.las2peer.api.ServiceException;
+import i5.las2peer.api.logging.MonitoringEvent;
 import i5.las2peer.restMapper.RESTService;
 import i5.las2peer.restMapper.annotations.ServicePath;
 $Database_Import$
